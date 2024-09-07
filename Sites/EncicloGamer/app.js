10a8c54c62ecf5d7192d51fd60f7e8d394e80b43
founder-gif <joaopaulo.gif@gmail.com>
2024-09-07 17:33:42 -0400

    Ajuste tecnico

---
+++ Sites/EncicloGamer/app.js
@@ -0,0 +1,97 @@
+// ====================== VARIAVEIS ======================
+//----------- Variaveis de elemento ------------
+let divResultados = document.getElementById("lista-resultado")
+let detalhesJogo = document.getElementById("detalhesJogo")
+let inputBusca = document.getElementById("inputBusca")
+let listaJogos = jogos
+//----------- Variaveis de controle ------------
+let termoBusca = ''
+let resultados = ''
+let nomeJogo = ''
+let categJogo = ''
+let descJogo = ''
+let i = ''
+
+// ------Area para informar ações ao usuario--------------
+divResultados.innerHTML = ` <div class="semConteudo">Pesquise Algo...</div>`
+detalhesJogo.innerHTML = ` <div class="semConteudo">Selecione um item...</div>`
+// -------------------------------------------------------
+
+// ====================== FUNÇÕES ========================
+
+listaJogos.sort((a,b) => {
+    return a.nome.localeCompare(b.nome)
+})
+
+
+function buscar() {
+    //---------- Limpar areas de resultados-----------
+    divResultados.innerHTML = ``
+    resultados = ``
+    //---------- Guardar informações -----------------
+    termoBusca = inputBusca.value.toLowerCase()
+    //---------- Verificar e printar informações -----
+    verificarBusca(termoBusca)
+}
+
+function verificarBusca(texto) {
+    // Inicia um looping para verificar a lista
+    //Incrementa o valor 0 para i. i serve para criar um indice
+    i = 0
+    for (let item of jogos) {
+        // Normaliza todos os valores para lowerCase
+        nomeJogo = item.nome.toLowerCase()
+        categJogo = item.categoria.toLowerCase()
+        descJogo = item.descricao.toLowerCase()
+        //Verifica se o termo busca esta em algum lugar no banco de dados
+        if (nomeJogo.includes(termoBusca) || categJogo.includes(termoBusca) || descJogo.includes(termoBusca) || item.ano.includes(termoBusca)) {
+            //Adiciona uma div com os dados do jogo
+            resultados += `
+            <div class="jogos" onclick="descricaoJogo(${i})">
+                <h2>> ${item.nome}</h2>
+                <h3>${item.curiosidade}</h3>
+                <p>Categoria: ${categJogo}</p>
+                <p>Lançamento: ${item.ano}</p>
+            </div>
+            `
+        }
+        //Incrementa i com +1
+        i++
+    }
+    //Adiciona todas as div's criadas para dentro do html
+    divResultados.innerHTML = resultados
+}
+
+function descricaoJogo(indece) {
+
+    if(window.innerWidth < 800){
+        detalhesJogo.style.display = 'block'
+        detalhesJogo.style.position = 'fixed'
+    }
+
+
+    detalhesJogo.innerHTML = `
+        <div class="detalhesJogo">
+            <div class="headerJogo">
+                <button onclick="fecharDetalhes()" class="interface">x</button>
+                <div class="capa">   
+                    <img src="${jogos[indece].capa}">
+                </div>
+                <div class="textoHeader">   
+                    <h2>${jogos[indece].nome}</h2>
+                    <p>Categoria: ${jogos[indece].categoria}</p>
+                    <p>Desenvolvedora: ${jogos[indece].desenvolvedora}</p>
+                    <p>Lançamento: ${jogos[indece].ano}</p>
+                </div>
+            </div>
+            <div class="descricaoJogo">
+                <p>${jogos[indece].descricao}</p>
+            </div>
+        </div>
+    `
+
+}
+
+function fecharDetalhes(){
+    detalhesJogo.style.display = 'none'   
+}
---
+++ Sites/EncicloGamer/readme.md
@@ -0,0 +1,33 @@
+# EncicloGamer
+Este projeto, desenvolvido durante a **Imersão Dev da Alura com Gemini**, tem como objetivo criar um **banco de dados** online de **jogos** retrô, com foco em resumir a história e curiosidades. A interface do usuário **simula** um **terminal monocromático** verde, proporcionando uma experiência imersiva.
+
+## Tecnologias Utilizadas:
+**HTML:** Estruturação da página e conteúdo.   
+**CSS:** Estilização da página, com foco na criação do visual de terminal verde.   
+**JavaScript:** Interatividade, manipulação do DOM, e busca de informações.   
+
+## Funcionalidades:
+**Interface de terminal:** Simula um terminal verde com comandos básicos para navegação e interação.   
+**Pesquisa:** Possibilidades de pesquisar os jogos usando termos como nome, ano de lançamento, categoria, desenvolvedora...    
+**Detalhes:** Area voltada para descrever melhor cada jogo selecionado.   
+## Em andamento:
+**Link de wiki:** Estou trabalhando em um botão para enviar o usuario para a fanwiki especifica do jogo. Parece simples, mas é um processo demorado.   
+**Tags:** Por motivos de cansaço mental, acabei esquecendo de aplicar as tags nos objetos do arquivo dados.js, mas graças a ultima aula da imersão meus belissimos instrutores me lembraram dessa possibilidade.   
+
+## Estrutura do Projeto:
+**index.html:** Página principal do projeto.   
+**style.css:** Arquivo de estilos CSS, responsável pela aparência visual.   
+**app.js:** Arquivo JavaScript, responsável pela lógica do projeto.   
+**dados.js:** Arquivo JavaScript contendo os dados dos jogos.   
+
+## Próximos Passos:
+**Responsividade:** Futuramente levar esse conteudo para palma da sua mão atraves da adaptação de tela.   
+Expandir o banco de dados: Adicionar mais jogos e informações detalhadas.   
+**Implementar novas funcionalidades:** Adicionar recursos como favoritos, comentários e compartilhamento nas redes sociais.   
+**Criar uma API:** Permitir que outros desenvolvedores acessem os dados do banco de dados.   
+**Area do administrador:** Permitir a adição dos dados dos jogos atraves do html com acesso exclusivo de uma conta adm.   
+
+## Agradecimentos:
+Agradecemos à **Alura** e à comunidade Gemini pela oportunidade de aprender e desenvolver este projeto.
+
+*Hashtags: #Alura #Gemini #JavaScript #HTML #CSS #RetroGames #Terminal #Dev*
