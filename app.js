6accbd0e3338d4535e985d4097f6a842e92489f9
founder-gif <joaopaulo.gif@gmail.com>
2024-09-07 21:46:12 -0400

    Melhoria de pesquisa e botões

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ let descJogo = ''
 let i = ''
 
 // ------Area para informar ações ao usuario--------------
-divResultados.innerHTML = ` <div class="semConteudo">Pesquise Algo...</div>`
-detalhesJogo.innerHTML = ` <div class="semConteudo">Selecione um item...</div>`
+divResultados.innerHTML = ` <div class="semConteudo">Pesquise um jogo...</div>`
+detalhesJogo.innerHTML = ` <div class="semConteudo">Selecione um jogo...</div>`
 // -------------------------------------------------------
 
 // ====================== FUNÇÕES ========================
 
-listaJogos.sort((a,b) => {
+listaJogos.sort((a, b) => {
     return a.nome.localeCompare(b.nome)
 })
 
@@ -38,33 +38,67 @@ function verificarBusca(texto) {
     // Inicia um looping para verificar a lista
     //Incrementa o valor 0 para i. i serve para criar um indice
     i = 0
+    
+    if (!texto) {
+        
+        divResultados.innerHTML = ` <div class="semConteudo">Insira o nome de um jogo...</div>`
+        
+    } else {
+        for (let item of jogos) {
+            // Normaliza todos os valores para lowerCase
+            nomeJogo = item.nome.toLowerCase()
+            categJogo = item.categoria.toLowerCase()
+            descJogo = item.descricao.toLowerCase()
+            //Verifica se o termo busca esta em algum lugar no banco de dados
+            if (nomeJogo.includes(termoBusca) || categJogo.includes(termoBusca) || descJogo.includes(termoBusca) || item.ano.includes(termoBusca)) {
+                //Adiciona uma div com os dados do jogo
+                resultados += `
+                <div class="jogos" onclick="descricaoJogo(${i})">
+                <h2>> ${item.nome}</h2>
+                <h3>${item.curiosidade}</h3>
+                <p>Categoria: ${categJogo}</p>
+                <p>Lançamento: ${item.ano}</p>
+                </div>
+                `
+            }
+            //Incrementa i com +1
+            i++
+        }
+        if (!resultados) {
+            divResultados.innerHTML = ` <div class="semConteudo">Nenhum jogo encontrado...</div>`
+        }
+        else {
+            //Adiciona todas as div's criadas para dentro do html
+            divResultados.innerHTML = resultados
+        }
+    }
+}
+function todos() {
+    i = 0
     for (let item of jogos) {
         // Normaliza todos os valores para lowerCase
         nomeJogo = item.nome.toLowerCase()
         categJogo = item.categoria.toLowerCase()
         descJogo = item.descricao.toLowerCase()
-        //Verifica se o termo busca esta em algum lugar no banco de dados
-        if (nomeJogo.includes(termoBusca) || categJogo.includes(termoBusca) || descJogo.includes(termoBusca) || item.ano.includes(termoBusca)) {
-            //Adiciona uma div com os dados do jogo
-            resultados += `
-            <div class="jogos" onclick="descricaoJogo(${i})">
-                <h2>> ${item.nome}</h2>
-                <h3>${item.curiosidade}</h3>
-                <p>Categoria: ${categJogo}</p>
-                <p>Lançamento: ${item.ano}</p>
-            </div>
+        //Adiciona uma div com os dados do jogo
+        resultados += `
+        <div class="jogos" onclick="descricaoJogo(${i})">
+        <h2>> ${item.nome}</h2>
+        <h3>${item.curiosidade}</h3>
+        <p>Categoria: ${categJogo}</p>
+        <p>Lançamento: ${item.ano}</p>
+        </div>
             `
-        }
         //Incrementa i com +1
         i++
     }
     //Adiciona todas as div's criadas para dentro do html
     divResultados.innerHTML = resultados
-}
 
+}
 function descricaoJogo(indece) {
 
-    if(window.innerWidth < 800){
+    if (window.innerWidth < 800) {
         detalhesJogo.style.display = 'block'
         detalhesJogo.style.position = 'fixed'
     }
@@ -92,6 +126,6 @@ function descricaoJogo(indece) {
 
 }
 
-function fecharDetalhes(){
-    detalhesJogo.style.display = 'none'   
+function fecharDetalhes() {
+    detalhesJogo.style.display = 'none'
 }
